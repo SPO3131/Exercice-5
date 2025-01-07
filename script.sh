5424279fd48deac8d61f5c1c73aecd02a2fff2ec
SPO3131 <stephane.polacco@free.fr>
2025-01-07 23:17:37 +0100

    Sauvegarde Bonus Condition

--- script.sh
+++ script.sh
@@ -12,8 +12,23 @@ cd $CONTENU
 # Creation d'un dossier
 mkdir Stockage
 
-# Creation d'un fichier dans le dossier "stockage"
-touch Stockage/collection.txt
+# Creation condition
+if [ -f collection.txt ]
+
+then
+# Demander a l'utilisateur le nom du film a rajouter
+echo "Quel est le nom du film a ajouter ?"
+
+# Creation variable pour récupérer le nom du film
+read FILM
+
+# Ajouter le nom du film dans le fichier "collection"
+echo $FILM>>collection.txt
+
+else 
+
+# Creation du fichier "collection"
+touch collection.txt
 
 # Demander a l'utilisateur le nom du film a rajouter
 echo "Quel est le nom du film a ajouter ?"
@@ -24,4 +39,5 @@ read FILM
 # Ajouter le nom du film dans le fichier "collection"
 echo $FILM>>collection.txt
 
+fi
 
